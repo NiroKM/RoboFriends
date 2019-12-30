4239bed8500161889da9c7732ec4ea2027e89936
Niroshan-Krishnamoorthy <niroshantrinity@gmail.com>
2019-12-30 10:50:14 +0530

    Adding the files

---
+++ src/components/CardList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({objects})=>{
+
+
+    return(     <div>
+                    {
+                        objects.map((name,i)=>{
+                            return <Card key={objects[i].id} 
+                                        id={objects[i].id} 
+                                        name={objects[i].name} 
+                                        email={objects[i].email}/>
+                        })
+                    }; 
+                </div>
+    );
+}
+
+export default CardList
